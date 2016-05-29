f16391a82a207f2bd029d5e8779ef8163cab6acb
Murachue <murachue+github@gmail.com>
2016-05-29 13:44:19 +0900

    n64/external.mk: fix message

--- n64/external.mk
+++ n64/external.mk
@@ -6,6 +6,7 @@ $(BINARIES_DIR)/n64linux.n64: $(BINARIES_DIR)/n64linux.bin
 	$(HOST_DIR)/usr/bin/chksum64 $(BINARIES_DIR)/n64linux.n64
 
 # TODO this requires linux and n64rom, add to deps, or user's responsibility (nconfig)?
+# note: MESSAGE macro's arg can not contain comma
 $(BINARIES_DIR)/n64linux.bin: world
-	@$(call MESSAGE,"Packing boot-loader, kernel, rootfs into the master file")
+	@$(call MESSAGE,"Packing boot-loader/kernel/rootfs into the master file")
 	$(HOST_DIR)/usr/bin/mkn64bin $(BINARIES_DIR)/n64linux.bin $(BINARIES_DIR)/n64load.bin $(BINARIES_DIR)/vmlinux.bin $(BINARIES_DIR)/rootfs.$(patsubst rootfs-%,%,$(TARGETS_ROOTFS))
