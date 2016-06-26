e094cee6f5b5e2aa890eacca8e7ec9fd8097a08f
Murachue <murachue+github@gmail.com>
2016-06-27 00:39:07 +0900

    n64/external.mk: use mkn64bin.iso9660 for iso9660 rootfs.

--- n64/external.mk
+++ n64/external.mk
@@ -7,6 +7,12 @@ $(BINARIES_DIR)/n64linux.n64: $(BINARIES_DIR)/n64linux.bin
 
 # TODO this requires linux and n64rom, add to deps, or user's responsibility (nconfig)?
 # note: MESSAGE macro's arg can not contain comma
+ifeq ($(TARGETS_ROOTFS),rootfs-iso9660)
+$(BINARIES_DIR)/n64linux.bin: world
+	@$(call MESSAGE,"Packing boot-loader/kernel/rootfs into the master file")
+	$(HOST_DIR)/usr/bin/mkn64bin.iso9660 $(BINARIES_DIR)/n64linux.bin $(BINARIES_DIR)/n64load.bin "BOOT/VMLINUX.BIN;1" $(BINARIES_DIR)/rootfs.$(patsubst rootfs-%,%,$(TARGETS_ROOTFS))
+else
 $(BINARIES_DIR)/n64linux.bin: world
 	@$(call MESSAGE,"Packing boot-loader/kernel/rootfs into the master file")
 	$(HOST_DIR)/usr/bin/mkn64bin $(BINARIES_DIR)/n64linux.bin $(BINARIES_DIR)/n64load.bin $(BINARIES_DIR)/vmlinux.bin $(BINARIES_DIR)/rootfs.$(patsubst rootfs-%,%,$(TARGETS_ROOTFS))
+endif
