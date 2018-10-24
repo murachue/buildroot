b312d8a1c2e9b6f2b71e8a0fa63b78e5c0aa93b4
Murachue <murachue+github@gmail.com>
2018-10-25 00:16:19 +0900

    external.mk: support multi rootfs (prefer iso9660)

--- n64/external.mk
+++ n64/external.mk
@@ -7,10 +7,10 @@ $(BINARIES_DIR)/n64linux.n64: $(BINARIES_DIR)/n64linux.bin
 
 # TODO this requires linux and n64rom, add to deps, or user's responsibility (nconfig)?
 # note: MESSAGE macro's arg can not contain comma
-ifeq ($(TARGETS_ROOTFS),rootfs-iso9660)
+ifneq ($(filter rootfs-iso9660,$(TARGETS_ROOTFS)),)
 $(BINARIES_DIR)/n64linux.bin: world
 	@$(call MESSAGE,"Packing boot-loader/kernel/rootfs into the master file")
-	$(HOST_DIR)/usr/bin/mkn64bin.iso9660 $(BINARIES_DIR)/n64linux.bin $(BINARIES_DIR)/n64load.bin "BOOT/VMLINUX.BIN;1" $(BINARIES_DIR)/rootfs.$(patsubst rootfs-%,%,$(TARGETS_ROOTFS))
+	$(HOST_DIR)/usr/bin/mkn64bin.iso9660 $(BINARIES_DIR)/n64linux.bin $(BINARIES_DIR)/n64load.bin "BOOT/VMLINUX.BIN;1" $(BINARIES_DIR)/rootfs.$(patsubst rootfs-%,%,$(filter rootfs-iso9660,$(TARGETS_ROOTFS)))
 else
 $(BINARIES_DIR)/n64linux.bin: world
 	@$(call MESSAGE,"Packing boot-loader/kernel/rootfs into the master file")
